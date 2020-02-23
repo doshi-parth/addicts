6b16caae8b91e8b5740f63f4824d4b598022798a
idhant96 <idhant123@gmail.com>
2020-02-23 01:33:53 -0700

    dsfsdfsd

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 const web = require('./routes/web');
 
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
