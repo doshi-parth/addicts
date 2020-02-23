7c718462630ab6c4bcc7db14fb30bbfc40e9fb9f
idhant96 <idhant123@gmail.com>
2020-02-23 04:20:00 -0700

    cors handled

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -1,6 +1,12 @@
 const twilio = require('twilio');
 
 exports.triggersms = (req, res) => {
+    req.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+
+
     var accountSid = 'AC43c3c5b7ed5240e1cf17d5847d389f81'; // Your Account SID from www.twilio.com/console
     var authToken = '3e4860215a426540e3b331c9561f33c7';   // Your Auth Token from www.twilio.com/console
 
@@ -14,7 +20,16 @@ exports.triggersms = (req, res) => {
     })
     .then((message) => {
         console.log(message.sid);
-        res.send({'status': 'success'});
+    })
+
+    client.messages.create({
+        body: 'Hello from idhant',
+        to: '4805775641',  // Text this number
+        from: '12055909366' // From a valid Twilio number
+    })
+    .then((message) => {
+        console.log(message.sid);
+        res.send('success')
     })
 
 }
--- controllers/streakController.js
+++ controllers/streakController.js
@@ -21,6 +21,9 @@ exports.addcontent = (req, res) => {
 }
 
 exports.getstreaks = (req, res) => {
+    req.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     let streaks = mongoose.model('Streak');
     streaks.find({}, (err, data) => {
         if(err){
@@ -29,6 +32,7 @@ exports.getstreaks = (req, res) => {
             return err;
           }
         if (data.length != 0){
+            trigger();
             res.send({"status":"success", "data": data});
         }
         else{
@@ -36,3 +40,47 @@ exports.getstreaks = (req, res) => {
         }
     })
 }
+
+
+
+
+// let msg = messages[1].body.split(" ")[1]
+
+function trigger(){
+
+    var accountSid = 'AC43c3c5b7ed5240e1cf17d5847d389f81'; // Your Account SID from www.twilio.com/console
+    var authToken = '3e4860215a426540e3b331c9561f33c7';   // Your Auth Token from www.twilio.com/console
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .list({
+         to: '+12055909366',
+         limit: 1
+       })
+      .then((messages) => {
+        let msg = messages[0].body.split(" ")[1];
+        // console.log(msg)
+        let streaks = new Streak(
+            {
+                date: Math.floor(Date.now() / 1000),
+                value: Number(msg),
+            }
+        );
+    
+        streaks.save(function (err) {
+            if (err) {
+                // res.send({"status": "Fail", "error": err});
+                console.log('failed at add record')
+            }
+            else{
+            // res.send({"status": "Success"});
+            console.log('success at add new record')
+            }
+        }) 
+      }
+          );
+
+}
+
+
+
