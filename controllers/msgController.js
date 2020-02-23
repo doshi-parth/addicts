3d2413a6f511a404e9dea455f07afdc8578cc986
idhant96 <idhant123@gmail.com>
2020-02-22 18:33:00 -0700

    new api added

---
+++ controllers/msgController.js
@@ -0,0 +1,23 @@
+const twilio = require('twilio');
+
+exports.triggersms = (req, res) => {
+    var accountSid = 'AC43c3c5b7ed5240e1cf17d5847d389f81'; // Your Account SID from www.twilio.com/console
+    var authToken = '3e4860215a426540e3b331c9561f33c7';   // Your Auth Token from www.twilio.com/console
+
+    // var twilio = require('twilio');
+    const client = new twilio(accountSid, authToken);
+
+    client.messages.create({
+        body: 'Hello from Node',
+        to: '4806163942',  // Text this number
+        from: '12055909366' // From a valid Twilio number
+    })
+    .then((message) => {
+        console.log(message.sid);
+        res.send({'status': 'success'});
+    })
+
+}
+
+
+
--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const story_controller = require('../controllers/storyController');
+const msg_controller = require('../controllers/msgController')
 
 // POST ROUTES
 router.post('/addstory', story_controller.addstory);
+router.post('/triggermsg', msg_controller.triggersms);
 
 
 //  GET ROUTES
